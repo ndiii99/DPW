4f3caeb74af2e4e0c3a7bc30c9524dc34bd37279
ndiii99 <62755707+ndiii99@users.noreply.github.com>
2021-03-06 14:19:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,64 @@
+﻿<html>
+<head>
+	<title>My Project</title>
+	<link rel="stylesheet" type="text/css"
+	media="all" href="style7.css">
+</head>
+<body>
+	<div id="layout">
+		<div id="header">
+		<img src="header.jpg" class="header">
+		</div>
+		<div id="menu">
+			<ul>
+				<li><a href="index7.html">My Project</a></li>
+				<li><a href="profil.html">About Me</a></li>
+				<li><a href="kontak.html">Ask Me</a></li>
+			</ul>
+			</div>
+		<div id="runningtext">
+		<marquee behavior="scroll" scrollamount="3" onmouseover="this.stop();" onmouseout="this.start();" direction="left">
+			Selamat Datang di Website Portofilio Saya
+		</marquee>
+		</div>
+		
+		<div id="isi">
+			<h2>Project Saya</h2>
+			
+			<table>
+			  <tr>
+				<th>No</th>
+				<th>Project</th>
+				<th>As</th>
+			  </tr>
+			  <tr>
+				<td>1</td>
+				<td>Quacking Earth</td>
+				<td>Full Stack</td>
+			  </tr>
+			  <tr>
+				<td>2</td>
+				<td>SIKEP Online</td>
+				<td>UI/UX Designer</td>
+			  </tr>
+			  <tr>
+				<td>3</td>
+				<td>Sharecode</td>
+				<td>Frontend</td>
+			  </tr>
+			  <tr>
+				<td>4</td>
+				<td>Child Care</td>
+				<td>Game Dev</td>
+			  </tr>
+			</table>
+		</div>
+		<div id="footer"> 
+				<a href="https://web.facebook.com/muhamad.randi.75">Facebook</a>
+				<a href="https://www.instagram.com/randut99/">Instagram</a>
+		</div>
+	</div>
+
+	
+</body>
+</html>
